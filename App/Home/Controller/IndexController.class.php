fb9b6fe00d8e3e4e88bdb2b84c04359170503667
Keifer.Gu <keifergu@gmail.com>
2016-03-08 21:10:22 +0800

    使用bower管理前端模块依赖,修改登录界面为原生bootstrap

--- App/Home/Controller/IndexController.class.php
+++ App/Home/Controller/IndexController.class.php
@@ -3,51 +3,6 @@ namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
 
-    public function test()
-    {
-        $sub = D('Count','Logic');
-        $sub->countPaperGrade('201004','111');
-        $this->display();
-    }
- 
-    public function set()
-    {
-        $LoginLogic = D('Login','Logic');
-        $studentID = $LoginLogic->apiLoginCheck();
-
-        $data = I('post.');
-        $StudentModel = D('Student','Logic');
-        switch ($data['type']) {
-            case 'course':
-                $result = $StudentModel->addCourse($studentID,$data['data']);
-                break;
-            
-            default:
-                $result = false;
-                break;
-        }
-        $this->ajaxReturn($result);
-    }
-
-    public function remove()
-    {
-        $LoginLogic = D('Login','Logic');
-        $studentID = $LoginLogic->apiLoginCheck();
-
-        $data = I("post.");
-        //此处应该对数据的合法性进行检查
-        $StudentModel = D('Student','Logic');
-        switch ($data['type']) {
-            case 'course':
-                $result = $StudentModel->removeCourse($studentID,$data['data']);
-                break;
-            
-            default:
-                $result = 'false';
-                break;
-        }
-        $this->ajaxReturn($result);
-    }
     public function index(){
        /*   $hashids = new \Common\Util\Hashids('Your Keys', 12);*/
     	$v = session('student.id');
@@ -79,7 +34,8 @@ class IndexController extends Controller {
     {
         $LoginLogic = D('Login','Logic');
         $studentID = $LoginLogic->loginCheck();
-
-
+        session('student_id', null);
+        $this->redirect('logout',U('Index/login'));
+        
     }
 }
