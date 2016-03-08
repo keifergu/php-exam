5f6502ce41df1f4a1a5bf041217d428df275f7f6
Keifer.Gu <keifergu@gmail.com>
2016-03-08 20:06:33 +0800

    将控制器文件从index中分离到三个独立的控制器中,修改view文件结构

--- App/Home/Controller/IndexController.class.php
+++ App/Home/Controller/IndexController.class.php
@@ -9,42 +9,7 @@ class IndexController extends Controller {
         $sub->countPaperGrade('201004','111');
         $this->display();
     }
-    public function question()
-    {   
-        $LoginLogic = D('Login','Logic');
-        $studentID = $LoginLogic->apiLoginCheck();
-
-        $paperID = I('post.paperID');
-        $num     = i('post.q');
-        $examLogic     = D('Exam','Logic');
-        $question      = $examLogic->getQuestion($paperID,$num);
-        $this->ajaxReturn($question);
-    }
-    public function getOldAnswer()
-    {
-        $LoginLogic = D('Login','Logic');
-        $studentID = $LoginLogic->apiLoginCheck();
-
-        $data      = I('post.');
-        $paperID      = $data['paperID'];
-        $num          = $data['num'];
-        $examLogic    = D('Exam','Logic');
-        $oldAnswer = $examLogic->getOldAnswer($studentID,$paperID,$num);
-        $this->ajaxReturn($oldAnswer);
-    }
-    public function submit()
-    {
-        $LoginLogic = D('Login','Logic');
-        $studentID = $LoginLogic->apiLoginCheck();
-
-        $data         = I('post.');
-        $paperID      = $data['paperID'];
-        $answer       = $data['answer'];
-        $num          = $data['num'];
-        $examLogic    = D('Exam','Logic');
-        $submitResult = $examLogic->submitAnswer($studentID,$paperID,$num,$answer);
-        $this->ajaxReturn($submitResult);
-    }
+ 
     public function set()
     {
         $LoginLogic = D('Login','Logic');
@@ -101,7 +66,7 @@ class IndexController extends Controller {
             $result = $StudentModel->loginUser($student_id,$password);
             if ($result) {
                 session('student_id',$student_id);
-                $this->success('登录成功',U('Index/user'));
+                $this->success('登录成功',U('User/index'));
             }else{
                 $this->error('登录失败',U('Index/login'));
             }
@@ -116,100 +81,5 @@ class IndexController extends Controller {
         $studentID = $LoginLogic->loginCheck();
 
 
-    }
-    public function user(){
-    	$LoginLogic = D('Login','Logic');
-        $studentID = $LoginLogic->loginCheck();
-        if ($studentID == null) {
-            $this->error('用户未登录',U('Index/login'));
-        }
-
-
-        $studentLogic = D('Student','Logic');
-        $paperList    = $studentLogic->getPaperList($studentID);
-        foreach ($paperList as $key => $value) {
-            $testUrl    = U('Index/exam_start',array('paperID' => $key));
-            $testData[] = array('testId'=> $key,'testName'=>$value,'examUrl'=>$testUrl);
-        }
-        $finishPaper = $studentLogic->getFinishPaper($studentID);
-        foreach ($finishPaper as $key => $value) {
-            //$testUrl    = U('Index/exam_start',array('paperID' => $key));
-            $finishPaperData[] = array('testId'=> $key,'testName'=>$value);
-        }
-        $studentInfo = $studentLogic->getStudentInfo($studentID);
-        $courseInfo   = $studentLogic->getAllCourse();
-        $gradeInfo     = $studentLogic->getAllGrade($studentID);
-        $this->assign('grade',$gradeInfo);
-        $this->assign('course',$courseInfo);
-        $this->assign('user',$studentInfo);
-        $this->assign('testData',$testData);
-        $this->assign('finishPaper',$finishPaperData);
-        $this->display();
-    }
-
-    public function exam_start($paperID){
-        $LoginLogic = D('Login','Logic');
-        $studentID = $LoginLogic->loginCheck();
-        if ($studentID == null) {
-            $this->error('用户未登录',U('Index/login'));
-        }
-
-        //获得试卷信息
-        $paperModel = D('Exam','Logic');
-        $paperInfo  = $paperModel->getPaperInfo($paperID);
-        $dictModel  = D('Dict');
-        $paperInfo['course_name'] = $dictModel->getName($paperInfo['course_id']);
-        $paperInfo['url'] = U('Index/exam',array('paperID'=>$paperID)); //生成跳转url
-
-        //将试卷信息存入cookie
-        $paperinfo = cookie('paperinfo');
-        $test_id = $paperinfo['test_id'];
-        if ($test_id == null) {
-            $cookieInfo = array('paper_id'   => $paperInfo['paper_id'],
-                                'total_num'  => $paperInfo['question_num'],
-                                'total_time' => $paperInfo['test_time']);
-            cookie('paperinfo' , $cookieInfo);
-        }
-        $this->assign('paper',$paperInfo);
-        $this->display();
-    }
-
-    public function exam(){
-        $LoginLogic = D('Login','Logic');
-        $studentID = $LoginLogic->loginCheck();
-        if ($studentID == null) {
-            $this->error('用户未登录',U('Index/login'));
-        }
-
-        $paperID       = I('get.paperID');
-        $examLogic     = D('Exam','Logic');
-        $examLogic->setStartTime($studentID,$paperID);
-
-        $paperInfo  = $examLogic->getPaperInfo($paperID);
-        $this->assign('paper',$paperInfo);
-        $this->display();
-    }
-
-
-
-    public function finish(){
-        $LoginLogic = D('Login','Logic');
-        $studentID = $LoginLogic->loginCheck();
-        if ($studentID == null) {
-            $this->error('用户未登录',U('Index/login'));
-        }
-
-        $paperID   = I('get.paperID');
-        $countLogic = D('Count','Logic');
-        $result = $countLogic->countPaperGrade($paperID,$studentID);
-        if ($result == false) {
-            $resultStr = "分数统计失败";
-        }else{
-            $resultStr = "分数统计成功";
-            $examLogic = D('Exam','Logic');
-            $examLogic->setEndTime($studentID,$paperID);
-        }
-        $this->assign('grade',$resultStr);
-    	$this->display();
     }
 }
